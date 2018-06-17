cd27df60b595ffe2f3ab68c18d6c8b3af8797679
Grzegorz Nawara <grzegorz.nawara@gmail.com>
2018-06-17 15:42:24 +0200

    init

---
+++ src/appsync.js
@@ -0,0 +1,6 @@
+﻿export default {
+    "graphqlEndpoint": "https://a2fembqhyve3lf2oyei22t3d7a.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "region": "eu-central-1",
+    "authenticationType": "API_KEY",
+    "apiKey": "da2-wpsfvec5zjd3pkknmaqk5fzuyu"
+}
