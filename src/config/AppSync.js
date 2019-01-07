029064de9ecdf8efc46d0f5e865eeea8cd9536a8
Grzegorz Nawara <grzegorz.nawara@gmail.com>
2019-01-07 16:48:28 +0100

    Fix keys

--- src/config/AppSync.js
+++ src/config/AppSync.js
@@ -2,5 +2,5 @@ export default {
     "graphqlEndpoint": "https://jj27lfuucjc4rla3as7jzinevq.appsync-api.eu-central-1.amazonaws.com/graphql",
     "region": "eu-central-1",
     "authenticationType": "API_KEY",
-    "apiKey": "da2-nopsvaoutfb3blduekxfa6diia"
+    "apiKey": "da2-vr7makcdw5cdjktkkxj2546c7a"
 }
