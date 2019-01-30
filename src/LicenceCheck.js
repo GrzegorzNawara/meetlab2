e0fae9fbfe7d4305d9718591e450d28fc7a08215
Grzegorz Nawara <grzegorz.nawara@gmail.com>
2019-01-30 09:08:50 +0100

    New licencing

---
+++ src/LicenceCheck.js
@@ -0,0 +1,23 @@
+//import React from 'react';
+//import debug from './debug'
+
+const LicenceCheck = ({setMG}) => {
+
+  localStorage.removeItem('mg','');
+  if(localStorage.getItem('licence'))
+    fetch('http://api.ignifer-labs.com/licence.php?licence='+localStorage.getItem('licence'), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(response => {
+      if(response) localStorage.setItem('mg',response);
+      setMG(response);
+    });
+
+  return null
+}
+
+export default LicenceCheck;
