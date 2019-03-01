4fd46af20e6d1b3c01f52bf9eef8a72dceb3061d
Grzegorz Nawara <grzegorz.nawara@gmail.com>
2019-03-01 07:14:25 +0100

    Fixing Mir sprint bricks

--- src/components/MirBrick.js
+++ src/components/MirBrick.js
@@ -6,8 +6,7 @@ const MirBrick = ({ title, subtitle, score, look, id, running, workshopId, raven
   <div className="col-12 px-1 align-items-center">
     <a href={'http://www.mir91.net/sim/#/'+workshopId+'/'+title+'/'+localStorage.getItem('me')}>
       <div className="text-center rounded p-3 px-2 m-1"
-           style={(Number(Date.now().toString())-Number(sort)>60*60*1000 ||
-             (!running))?cssStyles['lookSimulationOver']:cssStyles[look]} >
+           style={(!running)?cssStyles['lookSimulationOver']:cssStyles[look]} >
         <h2 className="text-nowrap">
           {(!running)?<img style={{height:'16px', marginRight:'15px', verticalAlign:'baseline'}} src="./images/lock-img.png" alt="Locked"/>:null}
           {'Sprint '+title}
