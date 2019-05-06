6999c8622facba0832159bb9f69bdce3d2375ab9
Grzegorz Nawara <grzegorz.nawara@gmail.com>
2019-05-06 16:20:23 +0200

    Fix for Fast api

--- src/database/mirSaveMsg.js
+++ src/database/mirSaveMsg.js
@@ -6,6 +6,12 @@ const saveMsg = (data) => {
     body: JSON.stringify(data)
   }).then(result => result.json())
     .then(result => result);
+
+  fetch('http://api.ignifer-labs.com/mir-sim/saveMsg.php',{
+    method: 'post',
+    body: JSON.stringify(data)
+  }).then(result => result.json())
+    .then(result => result);
 }
 
 export default saveMsg
